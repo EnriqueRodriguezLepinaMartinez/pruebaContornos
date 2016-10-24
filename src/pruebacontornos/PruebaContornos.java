95303594814f5d8929571cb1f9baf7da7f0a3a3a
erodriguez-lepinamartinez <erodriguez-lepinamartinez@atanasoff17.danielcastelao.org>
2016-10-24 08:55:09 +0200

    Desenvolvimiento Prueba!

---
+++ src/pruebacontornos/PruebaContornos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacontornos;
+
+/**
+ *
+ * @author erodriguez-lepinamartinez
+ */
+public class PruebaContornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int var1=4;
+        int var2=4;
+        int total;
+        total = var1 + var2;
+        System.out.println(total);
+    }
+    
+}
